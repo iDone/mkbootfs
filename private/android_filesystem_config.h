713724393aafa4d3ee2466bb05f4992fcc92bd16
osm0sis <osm0sis@outlook.com>
2016-10-05 13:26:49 -0300

    private: add SuperSU special ramdisk dir/file rules to fs config

--- private/android_filesystem_config.h
+++ private/android_filesystem_config.h
@@ -282,6 +282,9 @@ struct fs_path_config {
 */
 
 static const struct fs_path_config android_dirs[] = {
+    /* SuperSU ramdisk special directory */
+    { 00000, AID_ROOT,   AID_ROOT,   0, ".subackup" },
+
     { 00770, AID_SYSTEM, AID_CACHE,  0, "cache" },
     { 00500, AID_ROOT,   AID_ROOT,   0, "config" },
     { 00771, AID_SYSTEM, AID_SYSTEM, 0, "data/app" },
@@ -324,6 +327,12 @@ static const struct fs_path_config android_files[] = {
     { 00755, AID_ROOT,      AID_ROOT,      0, "init.class_main.sh" },
     { 00400, AID_ROOT,      AID_ROOT,      0, "module_hashes" },
     { 00750, AID_ROOT,      AID_ROOT,      0, "xbin/qe" },
+
+    /* SuperSU ramdisk special files */
+    { 00640, AID_ROOT,      AID_ROOT,      0, ".subackup/0001_fstab.*" },
+    { 00750, AID_ROOT,      AID_ROOT,      0, ".subackup/0002_init*" },
+    { 00750, AID_ROOT,      AID_ROOT,      0, ".subackup/0003_init*" },
+    { 00700, AID_ROOT,      AID_ROOT,      0, "sbin/launch_daemonsu.sh" },
 
     { 00440, AID_ROOT,      AID_SHELL,     0, "system/etc/init.goldfish.rc" },
     { 00550, AID_ROOT,      AID_SHELL,     0, "system/etc/init.goldfish.sh" },
