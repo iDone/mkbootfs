master-hax/aosp-system-core

Add static UID for the webview zygote process.
This UID will never own any files.

Bug: 21643067
Change-Id: Ifc887b1033ed4564336c8552012fb6bd0de73c15

include/private/android_filesystem_config.h
@@ -94,6 +94,7 @@
 #define AID_NVRAM         1050  /* Access-controlled NVRAM */
 #define AID_DNS           1051  /* DNS resolution daemon (system: netd) */
 #define AID_DNS_TETHER    1052  /* DNS resolution daemon (tether: dnsmasq) */
+#define AID_WEBVIEW_ZYGOTE 1053 /* WebView zygote process */
 /* Changes to this file must be made in AOSP, *not* in internal branches. */
 
 #define AID_SHELL         2000  /* adb and debug shell user */
@@ -207,6 +208,7 @@ static const struct android_id_info android_ids[] = {
     { "nvram",         AID_NVRAM, },
     { "dns",           AID_DNS, },
     { "dns_tether",    AID_DNS_TETHER, },
+    { "webvie_zygote", AID_WEBVIEW_ZYGOTE, },
 
     { "shell",         AID_SHELL, },
     { "cache",         AID_CACHE, },

iDone/mkbootfs

private/android_filesystem_config.h
@@ -95,6 +95,7 @@
 #define AID_NVRAM         1050  /* Access-controlled NVRAM */
 #define AID_DNS           1051  /* DNS resolution daemon (system: netd) */
 #define AID_DNS_TETHER    1052  /* DNS resolution daemon (tether: dnsmasq) */
+#define AID_WEBVIEW_ZYGOTE 1053 /* WebView zygote process */
 /* Changes to this file must be made in AOSP, *not* in internal branches. */
 
 #define AID_THEMEMAN      1300  /* theme manager */
@@ -232,6 +233,7 @@ static const struct android_id_info android_ids[] = {
     { "nvram",         AID_NVRAM, },
     { "dns",           AID_DNS, },
     { "dns_tether",    AID_DNS_TETHER, },
+    { "webview_zygote", AID_WEBVIEW_ZYGOTE, },
 
     { "shell",         AID_SHELL, },
     { "cache",         AID_CACHE, },