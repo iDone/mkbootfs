1f3e00bb7d72d553c7ab53bc7556f75ba1adf226
osm0sis <osm0sis@outlook.com>
2017-08-05 19:55:56 -0300

    fix native compiles by using included capabilities only - this was happening before https://github.com/osm0sis/mkbootfs/commit/7a2b039c4c4f5614cb0a89512ff0a5daaaee2814 anyway without realizing it due to the HAVE_ANDROID_OS ifdef failing

--- private/android_filesystem_config.h
+++ private/android_filesystem_config.h
@@ -54,11 +54,7 @@
 #include <sys/types.h>
 #include <stdint.h>
 
-#if defined(__BIONIC__)
-#include <linux/capability.h>
-#else
 #include "android_filesystem_capability.h"
-#endif
 
 #define CAP_MASK_LONG(cap_name)  (1ULL << (cap_name))
 
